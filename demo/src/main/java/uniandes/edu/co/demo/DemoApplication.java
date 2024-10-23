00ce386dee5fe77ca54e699fbd2b42fdb74a2dc4
Thais Tamaio <77994638+ThaisTamaio@users.noreply.github.com>
2024-10-23 10:02:37 -0500

    Commit Inicial

--- demo/src/main/java/uniandes/edu/co/demo/DemoApplication.java
+++ demo/src/main/java/uniandes/edu/co/demo/DemoApplication.java
@@ -1,54 +1,14 @@
 package uniandes.edu.co.demo;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import uniandes.edu.co.demo.modelo.Bar;
-import uniandes.edu.co.demo.repository.BarRepository;
-import uniandes.edu.co.demo.repository.BarRepository.RespuestaGrupo;
 
-@ComponentScan({"uniandes.edu.co.demo.repository"})
 @SpringBootApplication
-public class DemoApplication  implements CommandLineRunner{
-
-	@Autowired
-	private BarRepository barRepository;
-
+public class DemoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@Override
-	public void run(String... strings) throws Exception{
-
-		//QUERIES
-		List<Bar> res = barRepository.buscarPorId(60);
-
-		for(Bar b: res){
-			System.out.println(b);
-		}
-		
-
-		//AGGREGATIONS
-		List<RespuestaGrupo> res2 = barRepository.darBaresPorCiudad();
-
-		for(RespuestaGrupo r: res2){
-			System.out.println(r.getCiudad());
-			System.out.println(r.getCantidad());
-		}
-
-		//INSERT / UPDATE
-		barRepository.save(new Bar(101, "Bar de prueba","Cali","Alto",2));
-
-		//Update
-		barRepository.aniadirBebidaABar(101, "Bebida de prueba 2", "aperitivo", 10, "diurno", 10);
 	}
 
 }
